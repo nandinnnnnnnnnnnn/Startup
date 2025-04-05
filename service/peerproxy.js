e0cb3fc56680d2cb9bca4949b555d74b31f70acf
Nandintsetseg Batsaikhan <nandintsetsegbatsaikhan@Nandintsetsegs-Laptop.local>
2025-04-05 17:35:28 -0600

    ws

---
+++ service/peerproxy.js
@@ -0,0 +1,31 @@
+const { WebSocketServer } = require('ws');
+
+function peerProxy(httpServer) {
+  const socketServer = new WebSocketServer({ server: httpServer });
+
+  socketServer.on('connection', (socket) => {
+    socket.isAlive = true;
+
+    socket.on('message', function message(data) {
+      socketServer.clients.forEach((client) => {
+        if (client !== socket && client.readyState === socket.OPEN) {
+          client.send(data);
+        }
+      });
+    });
+
+    socket.on('pong', () => {
+      socket.isAlive = true;
+    });
+  });
+
+  setInterval(() => {
+    socketServer.clients.forEach((client) => {
+      if (client.isAlive === false) return client.terminate();
+      client.isAlive = false;
+      client.ping();
+    });
+  }, 10000);
+}
+
+module.exports = { peerProxy };
